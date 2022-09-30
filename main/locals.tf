159d0060261d590b67d9257c87d72fcbf98ad208
Mostefa Kamal Lala <mostefa.lala@innocap.com>
2022-09-29 22:08:31 -0400

    Added nsg flows netw and diags

--- main/locals.tf
+++ main/locals.tf
@@ -14,45 +14,45 @@ locals {
   #   }
 
   subscription_names = {
-    sb_pfm_prod = "sb-pfm-prod-1a4d"
-    sb_pfm_stg  = "sb-pfm-stg-01"
-    sb_pfm_qa   = "sb-pfm-qa-1a4d"
-    sb_pfm_dev  = "sb-pfm-dev-1a4d"
-    sb_id_prod  = "sb-Id-prod-1a4d"
-    sb_itt_prod  = "sb-itt-prod-1a4d"
-    sb_dvp_prod  = "sb-dvp-prod-1a4d"
-    sb_itm_prod  = "sb-itm-prod-1a4d"
-    sb_sec_prod  = "sb-sec-prod-01"
-    sb_cpo_prod_us  = "sb-cpo-prod-us-1a4d"
+    sb_pfm_prod    = "sb-pfm-prod-1a4d"
+    sb_pfm_stg     = "sb-pfm-stg-01"
+    sb_pfm_qa      = "sb-pfm-qa-1a4d"
+    sb_pfm_dev     = "sb-pfm-dev-1a4d"
+    sb_id_prod     = "sb-Id-prod-1a4d"
+    sb_itt_prod    = "sb-itt-prod-1a4d"
+    sb_dvp_prod    = "sb-dvp-prod-1a4d"
+    sb_itm_prod    = "sb-itm-prod-1a4d"
+    sb_sec_prod    = "sb-sec-prod-01"
+    sb_cpo_prod_us = "sb-cpo-prod-us-1a4d"
   }
 
   subscriptions_map = {
-    sb_pfm_prod = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_pfm_prod)]
-    sb_pfm_stg  = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_pfm_stg)]
-    sb_pfm_qa   = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_pfm_qa)]
-    sb_pfm_dev  = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_pfm_dev)]
-    sb_id_prod  = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_id_prod)]
-    sb_itt_prod  = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_itt_prod)]
-    sb_dvp_prod  = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_dvp_prod)]
-    sb_itm_prod  = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_itm_prod)]
-    sb_sec_prod  = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_sec_prod)]
-    sb_cpo_prod_us  = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_cpo_prod_us)]
+    sb_pfm_prod    = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_pfm_prod)]
+    sb_pfm_stg     = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_pfm_stg)]
+    sb_pfm_qa      = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_pfm_qa)]
+    sb_pfm_dev     = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_pfm_dev)]
+    sb_id_prod     = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_id_prod)]
+    sb_itt_prod    = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_itt_prod)]
+    sb_dvp_prod    = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_dvp_prod)]
+    sb_itm_prod    = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_itm_prod)]
+    sb_sec_prod    = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_sec_prod)]
+    sb_cpo_prod_us = local.subscriptions[index(local.subscriptions.*.name, local.subscription_names.sb_cpo_prod_us)]
   }
 
   spokes = {
-    sb_pfm_prod = local.subscriptions_map.sb_pfm_prod.spokes
-    sb_pfm_stg  = local.subscriptions_map.sb_pfm_stg.spokes
-    sb_pfm_qa   = local.subscriptions_map.sb_pfm_qa.spokes
-    sb_pfm_dev  = local.subscriptions_map.sb_pfm_dev.spokes
-    sb_id_prod  = local.subscriptions_map.sb_id_prod.spokes
-    sb_itt_prod  = local.subscriptions_map.sb_itt_prod.spokes
-    sb_dvp_prod  = local.subscriptions_map.sb_dvp_prod.spokes
-    sb_itm_prod  = local.subscriptions_map.sb_itm_prod.spokes
-    sb_sec_prod  = local.subscriptions_map.sb_sec_prod.spokes
-    sb_cpo_prod_us  = local.subscriptions_map.sb_cpo_prod_us.spokes
+    sb_pfm_prod    = local.subscriptions_map.sb_pfm_prod.spokes
+    sb_pfm_stg     = local.subscriptions_map.sb_pfm_stg.spokes
+    sb_pfm_qa      = local.subscriptions_map.sb_pfm_qa.spokes
+    sb_pfm_dev     = local.subscriptions_map.sb_pfm_dev.spokes
+    sb_id_prod     = local.subscriptions_map.sb_id_prod.spokes
+    sb_itt_prod    = local.subscriptions_map.sb_itt_prod.spokes
+    sb_dvp_prod    = local.subscriptions_map.sb_dvp_prod.spokes
+    sb_itm_prod    = local.subscriptions_map.sb_itm_prod.spokes
+    sb_sec_prod    = local.subscriptions_map.sb_sec_prod.spokes
+    sb_cpo_prod_us = local.subscriptions_map.sb_cpo_prod_us.spokes
   }
 
-  nsg_rg_rid_keys = [for sb_name in local.subscription_names : sb_name ]
+  sb_rid_keys = [for sb_name in local.subscription_names : sb_name]
 
 
   # config_files = fileset("../deployments/${var.location_code}/", "*configs.yaml")
---
+++ modules/nsg-log/main.tf
@@ -0,0 +1,41 @@
+resource "azurerm_network_watcher_flow_log" "netw_flow_logs" {
+  for_each             = { for nsg in var.nsgs : nsg.name => nsg }
+  network_watcher_name = var.network_watcher_name
+  resource_group_name  = each.value.resource_group_name
+  name                 = "flow-${each.key}"
+
+  network_security_group_id = each.value.id
+  storage_account_id        = var.storage_account_id
+  enabled                   = true
+
+  retention_policy {
+    enabled = true
+    days    = 7
+  }
+
+  traffic_analytics {
+    enabled               = true
+    workspace_id          = var.log_analytics_workspace.id
+    workspace_region      = var.log_analytics_workspace.location
+    workspace_resource_id = var.log_analytics_workspace.resource_id
+    interval_in_minutes   = 10
+  }
+}
+
+
+resource "azurerm_monitor_diagnostic_setting" "mdg" {
+  for_each                   = { for nsg in var.nsgs : nsg.name => nsg }
+  name                       = "diag-${each.key}"
+  target_resource_id         = each.value.id
+  storage_account_id         = var.storage_account_id
+  log_analytics_workspace_id = var.log_analytics_workspace.resource_id
+
+  log {
+    category = "AuditEvent"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+    }
+  }
+}
