7d17e37112a7d118739d547e0706b006427d403d
Mostefa Kamal Lala <mostefa.lala@innocap.com>
2022-10-05 00:12:59 -0400

    wip

--- modules/vnet-spoke/main.tf
+++ modules/vnet-spoke/main.tf
@@ -43,6 +43,7 @@ resource "azurerm_subnet" "subnets" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = each.value.address_prefixes
+  service_endpoints = each.value.service_endpoints
 
   depends_on = [
     azurerm_virtual_network.vnet
