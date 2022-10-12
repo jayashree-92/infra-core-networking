cba12b36f4fc78f0ae1baa021a967d062584e77d
Mostefa Kamal Lala <mostefa.lala@innocap.com>
2022-10-12 16:44:52 -0400

    Cleanups

--- main/outputs.tf
+++ main/outputs.tf
@@ -15,21 +15,6 @@ output "vnet_spokes" {
   }
 }
 
-# output "config_files" {
-#   value = local.config_files
-# }
-
-# output "merged_yamls" {
-#   value = local.merged_yamls
-# }
-
-# output "subscription_ids" {
-#   value = local.subscription_ids
-# }
-
-# output "subscriptions_map" {
-#   value = local.subscriptions_map
-# }
 output "create_vwan" {
   value = local.create_vwan
 }
@@ -37,15 +22,3 @@ output "create_vwan" {
 output "rmt_state" {
   value = local.create_vwan == true ? null : try(data.terraform_remote_state.vwan[0].outputs, null)
 }
-
-# output "vwan" {
-#   value = module.vwan
-# }
-
-# output "hubs" {
-#   value = module.hubs
-# }
-
-# output "vhub_ud_test" {
-#   value = module.hubs["vhub-net-prod-uc-01"].hub.id
-# }
