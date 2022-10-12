a7348f13ee89da64623eb159a213112a27118de5
Mostefa Kamal Lala <mostefa.lala@innocap.com>
2022-10-12 16:43:01 -0400

    PR fixes

---
+++ main/data.tf
@@ -0,0 +1,11 @@
+data "terraform_remote_state" "vwan" {
+  count   = local.create_vwan ? 0 : 1
+  backend = "azurerm"
+  config = {
+    storage_account_name = "terraformf157063802"
+    container_name       = "tfstate"
+    key                  = "eu.networking.terraform.tfstate"
+    access_key           = var.terraform_remote_state_access_key
+    subscription_id      = local.vwan_subscription.id
+  }
+}
