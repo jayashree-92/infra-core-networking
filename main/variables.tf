98aeb02cef0ea36a6e2924e894cf5bf0c0e92585
Mostefa Kamal Lala <mostefa.lala@innocap.com>
2022-12-16 09:56:02 -0500

    Removed defaults

--- main/variables.tf
+++ main/variables.tf
@@ -1,12 +1,10 @@
 variable "location_code" {
   description = "The short code of location to target."
   type        = string
-  default     = "eu"
 }
 
 variable "terraform_remote_state_access_key" {
   description = "Storage account access key for remote state."
   type        = string
-  default     = ""
   sensitive   = true
 }
