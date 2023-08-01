b471cbae07baaf081047307079c9d57423eb99f3
Hamza Errkiba <hamza.errkiba@innocap.com>
2023-08-01 15:55:33 +0000

    Updated variables.tf

--- modules/route-table/variables.tf
+++ modules/route-table/variables.tf
@@ -20,7 +20,7 @@ variable "routes" {
         name                   = string
         address_prefix         = string
         next_hop_type          = string
-        next_hop_in_ip_address = string
+        next_hop_in_ip_address = optional(string)
       })))
     })
   )
