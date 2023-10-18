072a67c4540ad6da0ef0ae16098b8ceca79f0de7
Emmanuel Legris <emmanuel.legris@innocap.com>
2023-10-18 17:37:41 +0000

    Merged PR 3113: First pipeline changes

--- main/datas.tf
+++ main/datas.tf
@@ -2,8 +2,8 @@ data "terraform_remote_state" "vwan" {
   count   = local.create_vwan ? 0 : 1
   backend = "azurerm"
   config = {
-    storage_account_name = "terraformf157063802"
-    container_name       = "tfstate"
+    storage_account_name = "sasectfspcuq9"
+    container_name       = "tfstate-core-networking"
     key                  = "eu.networking.terraform.tfstate"
     access_key           = var.terraform_remote_state_access_key
     subscription_id      = local.vwan_subscription.id
@@ -14,8 +14,8 @@ data "terraform_remote_state" "private_dns_zones" {
   count   = local.create_private_dns_zones ? 0 : 1
   backend = "azurerm"
   config = {
-    storage_account_name = "terraformf157063802"
-    container_name       = "tfstate"
+    storage_account_name = "sasectfspcuq9"
+    container_name       = "tfstate-core-networking"
     key                  = "cu.networking.terraform.tfstate"
     access_key           = var.terraform_remote_state_access_key
     subscription_id      = local.vwan_subscription.id
